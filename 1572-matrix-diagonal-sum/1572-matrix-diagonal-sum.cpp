6a4dc00aec4cd7bb6329b3c3518eb406a9b5a13a
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-02-01 15:08:29 -0500

    Time: 16 ms (71.63%), Space: 11.2 MB (58.18%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -4,10 +4,7 @@ public:
         int sum = 0;
         for (int i = 0; i < mat.size(); i++) {
             for (int j = 0; j < mat.size(); j++) {
-                if (i == j) {
-                    sum += mat[i][j];
-                }
-                if (i != j and i == mat.size() - 1 - j) {
+                if (i == j or i + j == mat.size() - 1) {
                     sum += mat[i][j];
                 }
             }
