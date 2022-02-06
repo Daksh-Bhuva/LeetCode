d1180c0071824978afd0bddb5c300b47ee226b1c
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-02-06 16:17:08 -0500

    Time: 7 ms (16.15%), Space: 7.8 MB (79.44%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        vector<int> count(101);
+        int sum = 0;
+        for (auto x : nums) {
+            count[x]++;
+            if (count[x] == 1)
+                sum += x;
+            else if (count[x] == 2) 
+                sum -= x;
+        }
+        return sum;
+    }
+};
