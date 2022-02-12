e6e012b3e60111eac55334112ecd1c6bfa1f4a6c
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-02-12 13:02:27 -0500

    Time: 3 ms (52.91%), Space: 6.3 MB (44.42%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        unordered_map<char,int> m;
+        vector<int> small(26,0), big(26,0);
         int ans = 0;
         for (int i = 0; i < jewels.length(); i++) {
-            m[jewels[i]]++;
+            if(jewels[i] - 'a' >= 0 and jewels[i] - 'a' < 26)
+                small[jewels[i] - 'a']++;
+            else
+                big[jewels[i] - 'A']++;
         }
         for (int i = 0; i < stones.length(); i++) {
-            if (m[stones[i]] == 1 )
-                ans++;
+            if (stones[i] - 'a' >= 0 and stones[i] - 'a'< 26 ) 
+                small[stones[i]  - 'a'] == 1 ? ans++ : ans += 0;
+            else 
+                big[stones[i] - 'A'] == 1 ? ans++ : ans += 0;
         }
         return ans;
     }
