e9fe82a13a94ad6f5db17befcb8d8828621b00fa
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-02-17 12:04:31 -0500

    Time: 7 ms (34.73%), Space: 7.3 MB (6.22%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int secondHighest(string s) {
-        int maxi = -1, maxi2 = -1;
         vector<int> v;
         
         for (int i = 0; i < s.length(); i++) {
             if ((s[i] - 'a' < 0) or (s[i] - 'a' > 25)) {
-                maxi = max(maxi,s[i] - '0');
                 v.push_back(s[i] - '0');
             }
         }
-        for (int i = 0; i < v.size(); i++) {
-            if (maxi != v[i]) {
-                maxi2 = max(maxi2, v[i]);
+        sort(v.begin(), v.end());
+        int count = 0;
+        for (int i = v.size() - 1; i >= 0; i--) {
+            while (i > 0 and v[i] == v[i - 1]) {
+                i--;
             }
+            count++;
+            if (count ==  2)
+                return v[i];
         }
-        return maxi2;
+        return -1;
     }
 };
