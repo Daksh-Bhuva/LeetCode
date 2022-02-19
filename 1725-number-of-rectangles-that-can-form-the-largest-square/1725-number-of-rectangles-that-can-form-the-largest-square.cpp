8b85215d26d2f705feff36bd3ef04eeca7599f01
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-02-19 12:17:18 -0500

    Time: 56 ms (54.32%), Space: 18.8 MB (41.64%) - LeetHub

--- 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.cpp
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.cpp
@@ -4,10 +4,7 @@ public:
         vector<int> squares;
         int count = 0;
         for (int i = 0; i < rectangles.size(); i++) {
-            int minim = INT_MAX;
-            for (int j = 0; j < 2; j++) {
-                minim = min(minim, rectangles[i][j]);
-            }
+            int minim = min(rectangles[i][0], rectangles[i][1]);
             squares.push_back(minim);
         }
         int maxi = *max_element(squares.begin(), squares.end());
