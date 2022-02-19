fef58a3f528da81f290bc0f0ab7deadd4c11d0f0
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-02-19 17:10:46 -0500

    Time: 0 ms (100.00%), Space: 6.4 MB (44.14%) - LeetHub

---
+++ 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeVowels(string s) {
+        string ans = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'a' or s[i] == 'e' or s[i] == 'i' or s[i] == 'o' or s[i] == 'u')
+                continue;
+            else
+                ans += s[i];
+        }
+        return ans;
+    }
+};
