992c545fdc40e52a082bbf14a079bb08f67bdb8d
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-02-19 19:27:57 -0500

    Time: 0 ms (100.00%), Space: 6.2 MB (15.46%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     string interpret(string command) {
-        unordered_map<string, string> m;
         string ans = "";
         for (int i = 0; i < command.length(); i++) {
             if (command[i] == '(' and command[i + 1] == ')')
