cad0493d95b16bb2c029cb0d4cbd1ad1d045e77b
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-02-20 23:52:30 -0500

    Time: 13 ms (44.69%), Space: 15 MB (93.78%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        string ans = "";
-        int count = 0, i = 0;
-        while (i < indices.size()) {
-            if (indices[i] == count) {
-                ans += s[i];
-                i = 0;
-                count++;
-            }
-            else {
-                i++;
-            }
-       }
+        string ans = s;
+        for (int i = 0; i < s.length(); i++) {
+            ans[indices[i]] = s[i];
+        }
         return ans;
     }
 };
