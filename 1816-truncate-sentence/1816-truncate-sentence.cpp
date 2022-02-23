cf5a1da5d553e24556e4e11d90b6745de4c8b3e3
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-02-22 22:33:24 -0500

    Time: 3 ms (49.91%), Space: 6.4 MB (58.82%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.cpp
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     string truncateSentence(string s, int k) {
-        vector<string> v;
-        string temp = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == ' ') {
-                v.push_back(temp);
-                temp = "";
-            }
-            else
-                temp += s[i];
-        }
-        v.push_back(temp);
-        string ans = v[0];
-        for (int i = 1; i < k; i++) {
-            ans = ans + ' ' + v[i];
-        }
-        return ans;
+        string ans = "";
+        int count = 0;
+        for(char c : s){
+            if (c == ' ')     
+                count++;
+            if (count == k)
+                break;
+            ans += c;
+        }        
+    return ans;  
     }
 };
