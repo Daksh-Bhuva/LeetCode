a0aced930b421fed7910afd88e346c5c265fa513
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-05-16 22:05:49 -0400

    Time: 982 ms (7.55%), Space: 163.9 MB (81.24%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -10,23 +10,57 @@
 
 class Solution {
 public:
+    
+    void dfs (TreeNode* tree, TreeNode* target, vector<int> &path, bool &present) {
+        if (tree == target) 
+            present = true;
+
+        if (!present and tree -> left != NULL) {
+            path.push_back(-1);
+            dfs(tree -> left, target, path, present);
+            if (!present) 
+                path.pop_back();
+        }
+        if (!present and tree -> right != NULL) {
+            path.push_back(1);
+            dfs(tree -> right, target, path, present);
+            if (!present) 
+                path.pop_back();
+        }
+    } 
+    
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        queue<TreeNode*> q;
-        q.push(cloned);
         
-        while(!q.empty()) {
-            int currSize = q.size();
-            for (int i = 0; i < currSize; i++) {
-                TreeNode* top = q.front();
-                q.pop();
-                if (top -> val == target -> val)
-                    return top;
-                if (top -> left != NULL)
-                    q.push(top -> left);
-                if (top -> right != NULL)
-                    q.push(top -> right);
+        vector<int> path;
+        bool present = false;
+        
+        dfs(original, target, path, present);
+        
+        for (int i : path) {
+            if (i == -1) {
+                cloned = cloned -> left;
+            }
+            else {
+                cloned = cloned -> right;
             }
         }
         return cloned;
+//         queue<TreeNode*> q;
+//         q.push(cloned);
+        
+//         while(!q.empty()) {
+//             int currSize = q.size();
+//             for (int i = 0; i < currSize; i++) {
+//                 TreeNode* top = q.front();
+//                 q.pop();
+//                 if (top -> val == target -> val)
+//                     return top;
+//                 if (top -> left != NULL)
+//                     q.push(top -> left);
+//                 if (top -> right != NULL)
+//                     q.push(top -> right);
+//             }
+//         }
+//         return cloned;
     }
 };
