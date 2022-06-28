673493dd14bd209f4266869252f58fd36b805347
Daksh Bhuva <97058737+Daksh-Bhuva@users.noreply.github.com>
2022-06-27 21:12:23 -0400

    Time: 72 ms (69.08%), Space: 17.4 MB (58.78%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -8,16 +8,11 @@ public:
         int ans = 0;
         unordered_set<int> st;
         for (int i = 0; i < 26; i++) {
-            while(v[i] > 0) {
-                if (st.count(v[i])) {
-                    v[i]--;
-                    ans++;
-                }
-                else {
-                    st.insert(v[i]);
-                    break;
-                }
+            while(v[i] > 0 and st.count(v[i])) {
+                v[i]--;
+                ans++;
             }
+            st.insert(v[i]);
         }
         return ans;
     }
